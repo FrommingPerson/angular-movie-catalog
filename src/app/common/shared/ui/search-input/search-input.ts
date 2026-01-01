7a164869f565038b71a9f58c277bde96efe2d395
isbeingused <maksim.nikiforovy123435@gmail.com>
2026-01-01 22:27:19 +0300

    updates in README.md and a small issue resolved

--- src/app/common/shared/ui/search-input/search-input.ts
+++ src/app/common/shared/ui/search-input/search-input.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Output, ViewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 @Component({
